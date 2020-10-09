50d30f438b95ad3097616300df02e4f8b0b205a1
Ted Oliver <ted@teds-mbp.attlocal.net>
2020-10-09 14:15:53 -0400

    Create basic solving operation

--- src/pegwinner/core.clj
+++ src/pegwinner/core.clj
@@ -10,7 +10,7 @@
 ;    6    7    8    9
 ; 10   11   12   13   14
 
-(defrecord BoardState [board moves])
+(defrecord BoardState [board prev-moves])
 
 (defn set-pegged
   "Set the plugged state of a hole"
@@ -32,25 +32,34 @@
 
 (defn accum-move
   "Make a move and add that move to the list of previous moves"
-  [{:keys [board moves]} from to]
-  (->BoardState (unjump board from to) (conj moves [from to])))
+  [{:keys [board prev-moves]} from to]
+  (->BoardState (unjump board from to) (conj prev-moves [from to])))
 
-(defn make-next-legal-moves [board-state]
+(defn make-next-legal-moves
   "Rewind a board from its current state to all possible past states"
-  (let [{:keys [board moves]} board-state
+  [board-state]
+  (let [{:keys [board prev-moves]} board-state
         legal-moves (ins/board-legal-moves board)]
-    (if (and (not (start-pos? board)) (empty? legal-moves))
+    (if (and (not (ins/start-pos? board)) (empty? legal-moves))
       []
       (map #(apply accum-move board-state %) legal-moves))))
+
+(def make-next-legal-moves-memo (memoize make-next-legal-moves))
+
+(defn find-winning-moves
+  "Find every sequence of moves that lead to these states"
+  [board-states]
+  (let [done? (ins/start-pos? (:board (first board-states)))]
+    (if done?
+      (map :prev-moves board-states)
+      (recur (mapcat make-next-legal-moves-memo board-states)))))
 
 (def filled-12 (insert-peg const/empty-board 12))
 (def filled-12-5 (insert-peg filled-12 5))
 (def full-board (reduce #(insert-peg %1 %2) const/empty-board (range 25)))
 (def top-empty (remove-peg full-board 0))
 
-(map show/print-board first-iteration)
-
-(make-next-legal-moves [filled-12 []])
+(def bs (->BoardState filled-12 []))
 
 (defn -main
   "I don't do a whole lot ... yet."
