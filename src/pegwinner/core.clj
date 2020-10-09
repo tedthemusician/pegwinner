7c96b7743ed63bcab46ce6f5e693279f20957427
Ted Oliver <ted@teds-mbp.attlocal.net>
2020-10-08 22:07:26 -0400

    Add fn descriptions; add tracked legal move ateempts

--- src/pegwinner/core.clj
+++ src/pegwinner/core.clj
@@ -9,40 +9,80 @@
 ;    6    7    8    9
 ; 10   11   12   13   14
 
-(defn set-pegged [state b n]
+(defn set-pegged
+  "Set the plugged state of a hole"
+  [state b n]
   (assoc-in b [n :plugged] state))
 
 (def remove-peg (partial set-pegged false))
 (def insert-peg (partial set-pegged true))
 
-(defn get-middle [b from to] (get-in b [from to]))
+(defn get-middle
+  "What hole are we (un)jumping over?"
+  [b from to]
+  (get-in b [from :moves to]))
 
 (defn plugged? [b n] (get-in b [n :plugged]))
 
-(defn legal? [b from to]
+(defn legal?
+  "Can we unjump from `from` to `to` on board `b`?"
+  [b from to]
   (let [middle (get-middle b from to)]
     (not (or (plugged? b middle) (plugged? b to)))))
 
 (defn hole-legal-moves [b n]
+  "Every legal move for a given hole"
   (let [moves (map first (get-in b [n :moves]))]
     (filter #(legal? b n %) moves)))
 
-(defn board-legal-moves [b]
-  (let [plugged (map first (filter (fn [[n v]] (:plugged v)) b))
-        hole-move-pairs (map (fn [n] [n (hole-legal-moves b n)]) plugged)]
-    (into {} hole-move-pairs)))
+(defn pairify
+  "Convert a `from` and a list of `tos` to a list of [`from` `to`] vectors.
+  In other words, a cartesian product where the first set has only one element."
+  [[from [& tos]]]
+  (map (fn [to] [from to]) tos))
 
-(defn unjump [b from to]
+(defn board-legal-moves
+  "Every legal move on the board"
+  [b]
+  (let [plugged (map first (filter (fn [[n v]] (:plugged v)) b))
+        hole-move-pairs (map (fn [n] [n (hole-legal-moves b n)]) plugged)
+        froms-with-tos (filter #(not (empty? (second %))) hole-move-pairs)]
+    (mapcat pairify froms-with-tos)))
+
+(defn unjump
+  "Take a board from its current state to the state before a jump"
+  [b from to]
   (let [middle (get-middle b from to)]
     (-> b
         (remove-peg from)
         (insert-peg middle)
         (insert-peg to))))
 
+(defn start-pos?
+  "Is this a starting position, i.e. is only one hole empty?"
+  [b]
+  (= 1 (count (filter :plugged (vals b)))))
+
+(defn accum-move
+  "Make a move and add that move to the list of previous moves"
+  [b prev-moves from to]
+  [(unjump b from to) (conj prev-moves [from to])])
+
+(defn make-next-legal-moves [[b prev-moves]]
+  (let [legal-moves (board-legal-moves b)]
+    (if (empty? legal-moves)
+      nil
+      (map #(apply accum-move b prev-moves %) legal-moves))))
+
+(make-next-legal-moves [filled-12 []])
+
 (def filled-12 (insert-peg const/empty-board 12))
 (def filled-12-5 (insert-peg filled-12 5))
+(def full-board (reduce #(insert-peg %1 %2) const/empty-board (range 25)))
+(def top-empty (remove-peg full-board 0))
 
-(def filled-board (reduce #(insert-peg %1 %2) const/empty-board (range 15)))
+(def first-iteration (make-legal-moves filled-12))
+(map show/print-board first-iteration)
 
 (defn -main
   "I don't do a whole lot ... yet."
--- src/pegwinner/show.clj
+++ src/pegwinner/show.clj
@@ -3,18 +3,31 @@
   (:require [pegwinner.constants :as const])
   (:gen-class))
 
-(defn get-row [b is] (map vector is (map (partial get b) is)))
+(defn get-row
+  "Get holes `is` from board `b`"
+  [b is]
+  (map vector is (map (partial get b) is)))
 
-(defn render-hole [[i h]] (if (:plugged h) (format "%2d " i) " . "))
+(defn render-hole
+  "If plugged, show the hole's index; otherwise show a dot"
+  [[i h]]
+  (if (:plugged h) (format "%2d " i) " . "))
 
-(defn render-row [r]
+(defn render-row
+  "Show indices or dots for a row. Indent the row as needed to show the board
+  as a triangle"
+  [r]
   (let [num-holes (count r)
         padding (str/join (replicate (- 5 num-holes) "  "))
         repr (str/join " " (map render-hole r))]
     (str padding repr))) 
 
-(defn render-board [b]
+(defn render-board
+  "Render a board as indices and dots in a triangle"
+  [b]
   (let [rows (map (partial get-row b) const/hole-groups)]
     (str/join "\n" (map render-row rows))))
 
-(defn print-board [b] (println (render-board b)))
+(defn print-board
+  "Print a board as indices and dots in a triangle"
+  [b] (println (render-board b)))
